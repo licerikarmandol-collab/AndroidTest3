ed118c1a11ae5deb758d878ca4d828cffb20ec05
armandolopez <edolo595@gmail,com>
2026-02-10 00:53:56 -0600

    update test

--- app/src/main/java/com/example/androidtest3/rickandmorty/ui/api/ApiClient.kt
+++ app/src/main/java/com/example/androidtest3/rickandmorty/ui/api/ApiClient.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface ApiClient {
 
-    @GET("character")
+    @GET("")
     suspend fun getAllCharacters(): Response<CharacterResponse>
 }
--- app/src/main/java/com/example/androidtest3/rickandmorty/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/androidtest3/rickandmorty/ui/screens/MainActivity.kt
@@ -8,16 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidtest3.rickandmorty.ui.navigation.AppNavHost
 import com.example.androidtest3.rickandmorty.ui.theme.AndroidTest3Theme
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
-@AndroidEntryPoint
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
